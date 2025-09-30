6dd5a25b1bcc3ba20b0cecf82b5130a5703d1d9e
shinghunhee <godgh@twave.co.kr>
2025-09-30 10:52:09 +0900

    fix : standalone 테스트 및 swcMinify 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'standalone',
+    swcMinify: true,  // 더 빠른 minify
+}
 
 module.exports = nextConfig
